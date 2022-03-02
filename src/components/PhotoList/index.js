2d36957e1e71e31210437d08b9fe12b06bfab9bd
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-03-02 18:59:51 -0500

    all tests pass

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import Modal from '../Modal'
+import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
-
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
 
   const [photos] = useState([
     {
@@ -124,17 +124,15 @@ const PhotoList = ({ category }) => {
 
   const currentPhotos = photos.filter(photo => photo.category === category);
 
-  const [currentPhoto, setCurrentPhoto] = useState();
-
   const toggleModal = (image, i) => {
-    setCurrentPhoto({...image, index: i});
+    setCurrentPhoto({ ...image, index: i });
     setIsModalOpen(!isModalOpen);
-  }
+  };
 
   return (
     <div>
       {isModalOpen && (
-        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+        <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
       )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
